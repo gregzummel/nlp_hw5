f781f6e34e375544027c860e84fa059cae857e1d
gregzummel <greg.szumel@gmail.com>
2017-04-13 12:40:41 -0400

    here it is

--- hw5.py
+++ hw5.py
@@ -1,19 +1,24 @@
-def readDocuments(document):
+def readDocuments(document_path):
+    import codecs
+    with codecs.open("corpus.txt", 'r', 'cp437') as file:
+        sentences = file.readlines()
 
-    #for every line:
-        #text == false
-        #if line == <TEXT>
-            #text == true
-        #if text and != beginning character != "<":
-            #add to doc
-        #elif text == "</TEXT"
-            #text == false
-
-    from nltk import ngrams
+    intext = False
     text = ""
-    n = 10
-    tengrams = ngrams(text.split(), n)
-    return tengrams
+    for sentence in sentences:
+        if intext:
+            text += sentence + " "
+        elif sentence == "<TEXT>":
+            intext == True
+        elif sentence == "</TEXT>":
+            intext == False
+
+
+    # from nltk import ngrams
+    # text = ""
+    # n = 10
+    # tengrams = ngrams(text.split(), n)
+    # return tengrams
 
 def compareVectors(a, b):
     #comparing two vectors to one another.
