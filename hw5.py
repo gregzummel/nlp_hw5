7aa239a5dd7ea6fb0429a7cd11c91fc6a59dd0b4
szuze <gregszumel@Gregs-MacBook-Pro-2.local>
2017-04-12 22:46:02 -0400

    Basic work

---
+++ hw5.py
@@ -0,0 +1,21 @@
+def readDocuments(document):
+
+    #for every line:
+        #text == false
+        #if line == <TEXT>
+            #text == true
+        #if text and != beginning character != "<":
+            #add to doc
+        #elif text == "</TEXT"
+            #text == false
+
+    from nltk import ngrams
+    text = ""
+    n = 10
+    tengrams = ngrams(text.split(), n)
+    return tengrames
+
+def compareVectors(a, b):
+    #comparing two vectors to one another.
+
+def findTopPassages():
